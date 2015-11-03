d0a79c558e187e853acea7f208d227a9ede11977
Alexey Belozerov <asbelozerov@gmail.com>
2015-11-03 23:54:51 +0600

    AMD and browser support

--- index.js
+++ index.js
@@ -17,36 +17,52 @@
 
 "use strict";
 
-/**
- * Constructs an enumeration with keys equal to their value.
- *
- * For example:
- *
- *   var COLORS = keyMirror({blue: null, red: null});
- *   var myColor = COLORS.blue;
- *   var isColorValid = !!COLORS[myColor];
- *
- * The last line could not be performed if the values of the generated enum were
- * not equal to their keys.
- *
- *   Input:  {key1: val1, key2: val2}
- *   Output: {key1: key1, key2: key2}
- *
- * @param {object} obj
- * @return {object}
- */
-var keyMirror = function(obj) {
-  var ret = {};
-  var key;
-  if (!(obj instanceof Object && !Array.isArray(obj))) {
-    throw new Error('keyMirror(...): Argument must be an object.');
-  }
-  for (key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      ret[key] = key;
-    }
-  }
-  return ret;
-};
+(function() {
+  var _global = this;
 
-module.exports = keyMirror;
+  /**
+   * Constructs an enumeration with keys equal to their value.
+   *
+   * For example:
+   *
+   *   var COLORS = keyMirror({blue: null, red: null});
+   *   var myColor = COLORS.blue;
+   *   var isColorValid = !!COLORS[myColor];
+   *
+   * The last line could not be performed if the values of the generated enum were
+   * not equal to their keys.
+   *
+   *   Input:  {key1: val1, key2: val2}
+   *   Output: {key1: key1, key2: key2}
+   *
+   * @param {object} obj
+   * @return {object}
+   */
+  var keyMirror = function (obj) {
+    var ret = {};
+    var key;
+    if (!(obj instanceof Object && !Array.isArray(obj))) {
+      throw new Error('keyMirror(...): Argument must be an object.');
+    }
+    for (key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        ret[key] = key;
+      }
+    }
+    return ret;
+  };
+
+  if (typeof(module) != 'undefined' && module.exports) {
+    // Publish as node.js module
+    module.exports = keyMirror;
+  } else  if (typeof define === 'function' && define.amd) {
+    // Publish as AMD module
+    define(function() {
+      return keyMirror;
+    });
+  } else {
+    // Publish as global (in browsers)
+    _global.keyMirror = keyMirror;
+  }
+
+})();
