8d873e922f61201662a5917ad581cd71e69e23c7
Patrick Taylor <patchfalcon+github@gmail.com>
2019-02-04 01:57:06 +0000

    Tests 🤓

---
+++ index.spec.js
@@ -0,0 +1,31 @@
+import assert from 'assert'
+
+import { keyMirrorPfx } from './'
+
+describe('KeyMirrorPrefix', () => {
+
+  it('should return a key mirror object', () => {
+    const testObject = keyMirrorPfx(null, {
+      hello: null,
+      world: null,
+    })
+
+    assert.deepEqual(testObject, {
+      hello: 'hello',
+      world: 'world',
+    })
+  })
+
+  it('should return a key mirror object with a prefix', () => {
+    const testObject = keyMirrorPfx('prefix', {
+      hello: null,
+      world: null,
+    })
+
+    assert.deepEqual(testObject, {
+      hello: 'prefix_hello',
+      world: 'prefix_world',
+    })
+  })
+
+})
